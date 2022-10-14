252a618cd0d49cb865f8d78b66bf1ac771bcf1aa
Chris Troutner <chris.troutner@gmail.com>
2022-10-13 18:39:02 -0700

    Fixing version of minimal-slp-wallet

--- src/html.js
+++ src/html.js
@@ -16,7 +16,7 @@ export default function HTML (props) {
         />
 
         {/* minimal-slp-wallet-web */}
-        <script src='https://unpkg.com/minimal-slp-wallet' />
+        <script src='https://unpkg.com/minimal-slp-wallet@5.2.1' />
 
         {/* bch-message-lib */}
         {/* <script src='https://unpkg.com/bch-message-lib' /> */}
